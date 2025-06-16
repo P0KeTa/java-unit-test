478fb48c876201c082bdd49efc7a375db4794475
DavidTaziasvili <Taziashvilidavid@mail.ru>
2025-06-16 15:04:08 +0400

    My second commit

--- src/test/java/ru/yandex/praktikum/CalculatorTest.java
+++ src/test/java/ru/yandex/praktikum/CalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import static org.mockito.Mockito.*;
 
 @RunWith(Parameterized.class)
 public class CalculatorTest {
